e0c6b02bd97715a61841814c4dcb0cc568c9c8e9
Matrozov <me@mear.me>
2017-03-31 20:58:42 +0300

    Base class

---
+++ src/SelectopusAsset.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace matrozov\selectopus;
+
+use yii\web\AssetBundle;
+
+/**
+ * SelectizeAsset
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class SelectopusAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/selectopus/dist';
+    public $css = [
+        'css/selectopus.min.css',
+    ];
+    public $js = [
+        'js/selectopus.min.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
---
+++ src/SelectopusWidget.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace matrozov\selectopus;
+
+class Selectopus extends \yii\widgets\InputWidget
+{
+    public $clientOptions;
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+
+    }
+}
